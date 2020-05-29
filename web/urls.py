f47fd2561d974e0901e8b7081b3191f852decff8
hamjihyeon <48716412+hamjihyeon@users.noreply.github.com>
2020-05-29 19:50:30 +0900

    modify list

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.show_list, name = 'show_list'),
+    path('list/<int:no>/<int:page>', views.show_list, name = 'show_list'),
     path('register/',views.register, name = 'register')
 ]
