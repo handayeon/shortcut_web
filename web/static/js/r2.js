37b0d5d9637306a60aadaf6d7f09a6e11fcdb113
hamjihyeon <48716412+hamjihyeon@users.noreply.github.com>
2020-06-01 21:10:10 +0900

    plus register bootstrap(70%)

--- web/static/js/r2.js
+++ web/static/js/r2.js
@@ -3,12 +3,11 @@
     //Row 추가
     function insRow() {
         oTbl = document.getElementById("regSynValue");
-        // placeholder = "위에서 입력한 값의 동의어를 입력하세요(예:복제하기, 똑같이 만들기...)"
         var oRow = oTbl.insertRow();
         
         var oCell = oRow.insertCell();
 
-        var frmTag = "<input type=text name=addText style=width:900px; height:50px placeholder='위에서 입력한 값의 동의어를 입력하세요(예:복제하기, 똑같이 만들기...)'>";
+        var frmTag = "<input type=text name=addText placeholder='위에서 입력한 값의 동의어를 입력하세요(예:복제하기, 똑같이 만들기...)'>";
 
         oCell.innerHTML = frmTag;
     }
