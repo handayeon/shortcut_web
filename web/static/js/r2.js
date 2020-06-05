a2d2ab0433f0e0ddbe4d92565603126daacb1e54
hamjihyeon <48716412+hamjihyeon@users.noreply.github.com>
2020-06-05 21:30:51 +0900

    update regitster

--- web/static/js/r2.js
+++ web/static/js/r2.js
@@ -1,18 +1,23 @@
 
-    var oTbl;
-    //Row 추가
-    function insRow() {
-        oTbl = document.getElementById("regSynValue");
-        var oRow = oTbl.insertRow();
+    // var oTbl;
+    // //Row 추가
+    // function insRow() {
+    //     oTbl = document.getElementById("regSynValue");
+    //     var oRow = oTbl.insertRow();
         
-        var oCell = oRow.insertCell();
+    //     var oCell = oRow.insertCell();
 
-        var frmTag = "<input type=text name=addText placeholder='위에서 입력한 값의 동의어를 입력하세요(예:복제하기, 똑같이 만들기...)'>";
+    //     var frmTag = "<input type=text name=addText placeholder='위에서 입력한 값의 동의어를 입력하세요(예:복제하기, 똑같이 만들기...)'>";
 
-        oCell.innerHTML = frmTag;
-    }
-   
-    //Row 삭제
-    function removeRow() {
-        oTbl.deleteRow(oTbl.clickedRowIndex);
-    }
+    //     oCell.innerHTML = frmTag;
+    // }
+    
+    function insRow(){
+		var div = document.createElement('div');
+		div.innerHTML = document.getElementById('regSynValue').innerHTML;
+		document.getElementById('field').appendChild(div);
+	}
+
+	function remove_item(obj){
+		document.getElementById('field').removeChild(obj.parentNode);
+	}
