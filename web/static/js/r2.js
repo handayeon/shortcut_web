534d86154a2cf4ec4d058c01e3a264fd06e1d37c
hamjihyeon <48716412+hamjihyeon@users.noreply.github.com>
2020-06-06 16:14:53 +0900

    update regitstr

--- web/static/js/r2.js
+++ web/static/js/r2.js
@@ -1,16 +1,4 @@
 
-    // var oTbl;
-    // //Row 추가
-    // function insRow() {
-    //     oTbl = document.getElementById("regSynValue");
-    //     var oRow = oTbl.insertRow();
-        
-    //     var oCell = oRow.insertCell();
-
-    //     var frmTag = "<input type=text name=addText placeholder='위에서 입력한 값의 동의어를 입력하세요(예:복제하기, 똑같이 만들기...)'>";
-
-    //     oCell.innerHTML = frmTag;
-    // }
     
     function insRow(){
 		var div = document.createElement('div');
@@ -19,5 +7,6 @@
 	}
 
 	function remove_item(obj){
+		// obj.parentNode 를 이용하여 삭제
 		document.getElementById('field').removeChild(obj.parentNode);
 	}
